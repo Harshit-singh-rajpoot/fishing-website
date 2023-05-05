5fe8386c06e7fefe2d12286e7f36de67527e4dff
Harshit-singh-rajpoot <131289912+Harshit-singh-rajpoot@users.noreply.github.com>
2023-05-05 22:31:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+
+function App() {
+  return (
+    <div>
+      <Component1/>
+      <Component2/>
+      
+    </div>
+  );
+}
+
+export default App;
